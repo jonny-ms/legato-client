199c603aac311ac912f126ddc08646285b892ece
CameronDunning <dunningcameron@gmail.com>
2019-10-26 17:35:17 -0400

    Add day click functionality

---
+++ src/components/Calendar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+
+class Calendar extends Component {
+  handleDateClick = arg => {
+    // bind with an arrow function
+    alert(arg.dateStr);
+  };
+
+  render() {
+    return (
+      <FullCalendar
+        dateClick={this.handleDateClick}
+        events={[
+          { title: "event 1", date: "2019-10-01" },
+          { title: "event 2", date: "2019-10-02" }
+        ]}
+        defaultView="dayGridMonth"
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      />
+    );
+  }
+}
+
+export default Calendar;
--- src/components/teachers/scheduleMonth.js
+++ src/components/teachers/scheduleMonth.js
@@ -1,30 +1,13 @@
 import React from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
-import interactionPlugin from "@fullcalendar/interaction";
+import Calendar from "../Calendar";
 
 const TeacherSchedule = () => {
-  // const handleDateClick = arg => {
-  //   // bind with an arrow function
-  //   alert(arg.dateStr);
-  // };
-
   return (
     <div className="EditTeacher">
       <p>Edit Teacher's schedule</p>
       <div>This is the teacher edit schedule form</div>
       <div>This will have a calendar to set the teachers schedule</div>
-      <FullCalendar
-        events={[
-          { title: "event 1", date: "2019-10-01" },
-          { title: "event 2", date: "2019-10-02" }
-        ]}
-        // dateClick={this.handleDateClick}
-        defaultView="dayGridMonth"
-        plugins={[dayGridPlugin]}
-      />
+      <Calendar />
     </div>
   );
 };
