9692acfe26d31a49e693447c226a0eb8285eb431
CameronDunning <dunningcameron@gmail.com>
2019-10-28 14:58:25 -0400

    Add calendar functionality

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,43 +2,109 @@ import React, { Component } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-// import listPlugin from "@fullcalendar/list";
+// listPlugin is to view a list of all appointments
+import listWeekPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
-
-const events = [
-  { title: "event 1", date: "2019-10-01" },
-  { title: "event 2", date: "2019-10-02" }
-];
 
 class Calendar extends Component {
   state = {
-    calendarEvents: [
-      // initial event data
-      { title: "Event Now", start: new Date() }
-    ]
+    calendarEvents: []
   };
 
-  handleDateClick = arg => {
-    if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
-      this.setState({
-        // add new event data
-        calendarEvents: this.state.calendarEvents.concat({
-          // creates a new array
-          title: "New Event",
-          start: arg.date,
-          allDay: arg.allDay
-        })
-      });
-    }
+  // handleDateClick = arg => {
+  //   // if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+  //   alert("Current view: " + arg.view.type);
+  //   arg.view.type = "timeGridDay";
+  //   // changeView("timeGrid");
+  //   this.setState({
+  //     // add new event data
+  //     calendarEvents: this.state.calendarEvents.concat({
+  //       // creates a new array
+  //       title: "New Event",
+  //       start: arg.date,
+  //       end: arg.date + 1800
+  //       // allDay: arg.allDay
+  //     })
+  //   });
+  //   console.log(arg.date);
+  //   console.log(arg.date + 1800);
+  //   // }
+  // };
+
+  handleSelect = arg => {
+    // console.log(arg);
+    const id = this.state.calendarEvents.length;
+    // console.log(id);
+    this.setState({
+      calendarEvents: this.state.calendarEvents.concat({
+        // creates a new array
+        title: "Available",
+        start: arg.start,
+        end: arg.end,
+        id: id
+      })
+    });
+  };
+
+  handleDrop = arg => {
+    const id = arg.oldEvent.id;
+    const events = this.state.calendarEvents;
+
+    events[id] = {
+      title: "available",
+      start: arg.event.start,
+      end: arg.event.end,
+      id: id
+    };
+
+    this.setState({
+      calendarEvents: events
+    });
+  };
+
+  handleResize = arg => {
+    console.log("dragging");
+    console.log(arg.event);
+    const id = arg.prevEvent.id;
+    const events = this.state.calendarEvents;
+
+    events[id] = {
+      title: "available",
+      start: arg.event.start,
+      end: arg.event.end,
+      id: id
+    };
+
+    this.setState({
+      calendarEvents: events
+    });
   };
 
   render() {
     return (
       <FullCalendar
-        dateClick={this.handleDateClick}
+        // dateClick={this.handleDateClick}
         events={this.state.calendarEvents}
-        defaultView="dayGridMonth"
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        defaultView="timeGridWeek"
+        header={{
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridWeek,listWeek"
+        }}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          listWeekPlugin,
+          interactionPlugin
+        ]}
+        selectable={true}
+        editable={true}
+        droppable={true}
+        draggable={true}
+        eventOverlap={false}
+        select={this.handleSelect}
+        eventDrop={this.handleDrop}
+        eventResize={this.handleResize}
       />
     );
   }
