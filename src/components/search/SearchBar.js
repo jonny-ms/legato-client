f091da9ed8caa320ab23125d2a6cf760cfafc15f
Aaron Tagadtad <aarontagadtad@Aaron-Tagadtads-MacBook-Pro.local>
2019-10-28 21:33:33 -0400

    added instrument and level filter

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,42 +1,71 @@
 import React, { useState } from "react";
 import Results from "./Results";
-import "./SearchBar.css"
 
 export default function SearchBar(props) {
   const [name, setName] = useState("");
   const [instrument, setInstrument] = useState("Select");
+  const [level, setLevel] = useState("Select")
+
+  const instruments = [
+    'Select',
+    'Accordion',
+    'Bassoon',
+    'Cello',
+    'Clarinet',
+    'Double bass',
+    'Drums',
+    'Euphonium',
+    'Flute',
+    'French horn',
+    'Guitar',
+    'Harp',
+    'Harpsichord',
+    'Oboe',
+    'Organ',
+    'Percussion',
+    'Piano',
+    'Recorder',
+    'Saxophone',
+    'Speech arts and drama',
+    'Trombone',
+    'Trumpet',
+    'Tuba',
+    'Viola',
+    'Violin',
+    'Voice'
+  ]
+
+  const levels = [
+    'Select',
+    'Beginner',
+    'Intermediate',
+    'Advance'
+  ]
 
   
   let filteredNames = props.teachers.filter(function(teacher) {
-    // console.log(teacher)
     let check = false;
-    if (!teacher.first_name.includes(name)) {
-      return false
+    if (!teacher.first_name.includes(name) 
+    || !teacher.courses.some(course => course.instrument === instrument)
+    || !teacher.courses.some(course => course.level === level)
+    ) {
+      if (instrument !== "Select" && level !== "Select") {
+        return false
+      }
     }
-    if (instrument == "Select") {
+    if (instrument === "Select" || level === "Select") {
       check = true
     } else if (teacher.courses.some(course => course.instrument === instrument)) {
+      check = true
+    } else if (teacher.courses.some(course => course.level === level)) {
       check = true
     } else {
       check = false
     }
-    return  check
+    return check
       
   })
   
-  // let filteredCourse = props.teachers.filter(teacher => {
-  //   for (const course of teacher.courses) {
-  //     if (instrument === course.instrument) {
-  //       return true
-  //     }
-  //   }
-  //   return false
-  // })
-
-
-
-
-
   return (
     <section className="search">
       <form className="search__form" onSubmit={event => event.preventDefault()}>
@@ -51,10 +80,15 @@ export default function SearchBar(props) {
       </form>
 
         <select onChange={e => setInstrument(e.target.value)}>
-          <option>Select</option>
-          <option>Piano</option>
-          <option>Flute</option>
-          <option>Drums</option>
+          {instruments.map((instrument) => {
+            return <option>{instrument}</option>
+          })}
+        </select>
+
+        <select onChange={e => setLevel(e.target.value)}>
+          {levels.map((level) => {
+            return <option>{level}</option>
+          })}
         </select>
 
       <div>
