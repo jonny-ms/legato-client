997c0f366040a3c3e570687ea5c172843ded5605
Aaron Tagadtad <aarontagadtad@Aaron-Tagadtads-MacBook-Pro.local>
2019-10-30 13:20:44 -0400

    added filters for instrument, name, level and rate

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -4,68 +4,95 @@ import Results from "./Results";
 export default function SearchBar(props) {
   const [name, setName] = useState("");
   const [instrument, setInstrument] = useState("Select");
-  const [level, setLevel] = useState("Select")
+  const [level, setLevel] = useState("Select");
+  const [rate, setRate] = useState("");
 
   const instruments = [
-    'Select',
-    'Accordion',
-    'Bassoon',
-    'Cello',
-    'Clarinet',
-    'Double bass',
-    'Drums',
-    'Euphonium',
-    'Flute',
-    'French horn',
-    'Guitar',
-    'Harp',
-    'Harpsichord',
-    'Oboe',
-    'Organ',
-    'Percussion',
-    'Piano',
-    'Recorder',
-    'Saxophone',
-    'Speech arts and drama',
-    'Trombone',
-    'Trumpet',
-    'Tuba',
-    'Viola',
-    'Violin',
-    'Voice'
-  ]
+    "Select",
+    "Accordion",
+    "Bassoon",
+    "Cello",
+    "Clarinet",
+    "Double bass",
+    "Drums",
+    "Euphonium",
+    "Flute",
+    "French horn",
+    "Guitar",
+    "Harp",
+    "Harpsichord",
+    "Oboe",
+    "Organ",
+    "Percussion",
+    "Piano",
+    "Recorder",
+    "Saxophone",
+    "Speech arts and drama",
+    "Trombone",
+    "Trumpet",
+    "Tuba",
+    "Viola",
+    "Violin",
+    "Voice"
+  ];
 
-  const levels = [
-    'Select',
-    'Beginner',
-    'Intermediate',
-    'Advance'
-  ]
+  const levels = ["Select", "Beginner", "Intermediate", "Advance"];
 
-  
+  function isTeacherNameIncluded(teacherName, teacher) {
+    let fullName = `${teacher.first_name} ${teacher.last_name}`;
+    if (!teacherName) {
+      return true;
+    }
+    if (fullName.toUpperCase().includes(teacherName.toUpperCase())) {
+      return true;
+    }
+    return false;
+  }
+
+  function isLevelIncluded(level, teacher) {
+    if (level === "Select") {
+      return true;
+    }
+    if (teacher.courses.some(course => course.level === level)) {
+      return true;
+    }
+    return false;
+  }
+
+  function isInstrumentIncluded(instrument, teacher) {
+    if (instrument === "Select") {
+      return true;
+    }
+    if (teacher.courses.some(course => course.instrument === instrument)) {
+      return true;
+    }
+    return false;
+  }
+
+  function isRateIncluded(rate, teacher) {
+    if (!rate) {
+      return true;
+    }
+    if (teacher.courses.some(course => course.rate <= rate)) {
+      return true;
+    }
+    return false;
+  }
+
   let filteredNames = props.teachers.filter(function(teacher) {
     let check = false;
-    if (!teacher.first_name.includes(name) 
-    || !teacher.courses.some(course => course.instrument === instrument)
-    || !teacher.courses.some(course => course.level === level)
+    if (
+      isTeacherNameIncluded(name, teacher) &&
+      isLevelIncluded(level, teacher) &&
+      isInstrumentIncluded(instrument, teacher) &&
+      isRateIncluded(rate, teacher)
     ) {
-      if (instrument !== "Select" && level !== "Select") {
-        return false
-      }
+      check = true;
     }
-    if (instrument === "Select" || level === "Select") {
-      check = true
-    } else if (teacher.courses.some(course => course.instrument === instrument)) {
-      check = true
-    } else if (teacher.courses.some(course => course.level === level)) {
-      check = true
-    } else {
-      check = false
-    }
-    return check
-      
-  })
-  
+    return check;
+  });
+  console.log("filteredNames: ", filteredNames);
+
   return (
     <section className="search">
       <form className="search__form" onSubmit={event => event.preventDefault()}>
@@ -77,19 +104,27 @@ export default function SearchBar(props) {
           type="text"
           onChange={event => setName(event.target.value)}
         />
+        <input
+          className="radius"
+          spellCheck="false"
+          placeholder="Set Rate"
+          name="search"
+          type="text"
+          onChange={event => setRate(event.target.value)}
+        />
       </form>
 
-        <select onChange={e => setInstrument(e.target.value)}>
-          {instruments.map((instrument) => {
-            return <option>{instrument}</option>
-          })}
-        </select>
+      <select onChange={e => setInstrument(e.target.value)}>
+        {instruments.map(instrument => {
+          return <option>{instrument}</option>;
+        })}
+      </select>
 
-        <select onChange={e => setLevel(e.target.value)}>
-          {levels.map((level) => {
-            return <option>{level}</option>
-          })}
-        </select>
+      <select onChange={e => setLevel(e.target.value)}>
+        {levels.map(level => {
+          return <option>{level}</option>;
+        })}
+      </select>
 
       <div>
         <ul>
