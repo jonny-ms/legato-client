fd645f7fe2033c11b2c07be25628975c9a8df7cb
CameronDunning <dunningcameron@gmail.com>
2019-11-02 20:09:51 -0400

    backup1

---
+++ src/components/teachers/editTeacher.js
@@ -0,0 +1,16 @@
+import React from "react";
+import EditProfile from "./EditProfile";
+
+const EditTeacher = () => {
+  return (
+    <div className="EditTeacher">
+      <p>Edit Teacher</p>
+      <div>This is the edit teacher form</div>
+      <div>This will have a form to create the teachers profile</div>
+
+      <EditProfile />
+    </div>
+  );
+};
+
+export default EditTeacher;
